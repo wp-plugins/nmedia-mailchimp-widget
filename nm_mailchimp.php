a2e190f143c8baf2162be30c38216edca73d0f0f
nmedia <nmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-31 07:48:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/nmedia-mailchimp-widget/tags/2.1@497826 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nm_mailchimp.php
+++ nm_mailchimp.php
@@ -3,7 +3,7 @@
 Plugin Name: Simple MailChimp Email List Subscriber
 Plugin URI: http://www.najeebmedia.com/wordpress-mailchimp-plugin-2-0-released/
 Description: Form to capture email subscriptions and send them to your MailChimp account list
-Version: 2.0
+Version: 2.1
 Author: Najeeb Ahmad
 Author URI: http://www.najeebmedia.com/
 */
