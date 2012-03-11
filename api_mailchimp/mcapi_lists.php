d042bd85bd29513d76f9004c7483ece8db57ac5e
nmedia <nmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-11 16:14:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/nmedia-mailchimp-widget/trunk@517524 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- api_mailchimp/mcapi_lists.php
+++ api_mailchimp/mcapi_lists.php
@@ -6,25 +6,12 @@ class and do some basic error checking.
 require_once 'inc/MCAPI.class.php';
 //require_once 'inc/config.inc.php'; //contains apikey
 
-$api = new nm_MCAPI(get_option('nm_mc_apikey'));
+$api = new nm_MCAPI(get_option('nm_mc_api_key'));
 
 $retval = $api->lists();
 
 if ($api->errorCode){
-	echo "Unable to load lists()!";
-	echo "\n\tCode=".$api->errorCode;
-	echo "\n\tMsg=".$api->errorMessage."\n";
+	_e("You did not enter API Keys please enter your API Keys from Nmedia Mailchimp Setting area");
 } 
-/*else {
-	echo "Lists that matched:".$retval['total']."\n";
-	echo "Lists returned:".sizeof($retval['data'])."\n";
-	foreach ($retval['data'] as $list){
-		echo "Id = ".$list['id']." - ".$list['name']."\n";
-		echo "Web_id = ".$list['web_id']."\n";
-		echo "\tSub = ".$list['stats']['member_count'];
-		echo "\tUnsub=".$list['stats']['unsubscribe_count'];
-		echo "\tCleaned=".$list['stats']['cleaned_count']."\n";
-		
-}*/
 
 ?>
