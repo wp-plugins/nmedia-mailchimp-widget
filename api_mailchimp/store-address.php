dae9fc9b995aea710866c6f2420a059cadc5d1a4
nmedia <nmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-11 16:14:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/nmedia-mailchimp-widget/tags/1.2@517524 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- api_mailchimp/store-address.php
+++ api_mailchimp/store-address.php
@@ -1,51 +1,113 @@
 <?php
-function storeAddress(){
+/*
+This script is written by Najeeb Ahmad, NajeebMedia.com
+
+postToMailChimp class is interface class between NMedia Mailchimp plugin and Mailchimp API class.
+Do not modify this.
+*/
+
+/*ini_set('display_errors',1);
+error_reporting(E_ALL);*/
+
+
+$path = $_POST['abs_path'] . 'wp-load.php';
+//echo $path; die;
+require( $path );
+
+class postToMailChimp
+{
+	var $email;
+	var $list_id;
+	var $full_name;
 	
-	// Validation
-	if(!$_POST['email']){ return "No email address provided"; } 
-
-	if(!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*$/i", $_POST['email'])) {
-		return "Email address is invalid"; 
-	}
-
-	require_once('MCAPI.class.php');
-	// grab an API Key from http://admin.mailchimp.com/account/api/
-	//$api = new MCAPI('1c75426364cad9ce8ec46a6aae650a87-us2');		//Nmedia mailchimp account
-	if($_POST['api_key'] == '')
+	var $api_key;
+	var $thanks;
+	
+	var $err;
+	
+	/*
+	** following function is saving subscriber data to mailchimp list
+	*/
+	function saveToList()
 	{
-		return 'No API key found, please set API from Widget section';
+		
+		require_once('MCAPI.class.php');
+	
+		$comm = new MCAPI($this -> api_key);		
+
+		
+		$names = explode(',', $this -> full_name);
+		
+		$mergeVars = array( 'FNAME'	=> $names['0'],
+							'LNAME'	=> $names['1']);
+		
+		if($comm->listSubscribe($this -> list_id, $this -> email, $mergeVars) === true) {
+			if($this -> thanks == '')
+				return 'Success! Check your email to confirm sign up.';
+			else
+				return $this -> thanks;
+		}else{
+			// An error ocurred, return error message	
+			return 'Error: ' . $comm->errorMessage;
+		}
 	}
 	
-	$api = new MCAPI($_POST['api_key']);		
+	
+	/*
+	** setting data posted here
+	*/
+	function setPostedData($email1, $list_id1, $fname1, $api_key1, $thanks1)
+	{
+		if(!$email1)
+		{ 
+			$this -> err = "Email is required";
+			return false;
+		} 
+
+		if(!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*$/i", $email1)) 
+		{
+			$this -> err =  "Email address is invalid"; 
+			return false;
+		}
+		
+		if($api_key1 == '')
+		{
+			$this -> err =  'No API key found, please set API from Widget section';
+			return false;
+		}
+		
+		if($list_id1 == '')
+		{
+			$this -> err =  'No List ID found, please set List ID from Widget section';
+			return false;
+		}
+		
+		
+		$this -> email   	= sanitize_text_field( $email1 );
+		$this -> list_id 	= sanitize_text_field( $list_id1 );
+		
+		if($fname1)
+			$this -> full_name = sanitize_text_field( $fname1 );
 			
-	// grab your List's Unique Id by going to http://admin.mailchimp.com/lists/
-	// Click the "settings" link for the list - the Unique Id is at the bottom of that page. 
-	//$list_id = "9582aab621";		//Nmedia Client List Id
-	if($_POST['list_id'] == '')
-	{
-		return 'No List ID found, please set List ID from Widget section';
+		$this -> api_key	= sanitize_text_field( $api_key1 );
+		$this -> thanks	= sanitize_text_field( $thanks1 );
+		
+		return true;
+		
+		//var_dump($this);
+		
 	}
-	
-	$list_id = $_POST['list_id'];
-	
-	// Merge variables are the names of all of the fields your mailing list accepts
-	// Ex: first name is by default FNAME
-	// You can define the names of each merge variable in Lists > click the desired list > list settings > Merge tags for personalization
-	// Pass merge values to the API in an array as follows
-	$mergeVars = array('FNAME'=>$_POST['fname'],
-					   'LNAME'=>$_POST['lname']);
-	
-	if($api->listSubscribe($list_id, $_POST['email'], $mergeVars) === true) {
-		// It worked!	
-		return 'Success! Check your email to confirm sign up.';
-	}else{
-		// An error ocurred, return error message	
-		return 'Error: ' . $api->errorMessage;
-	}
-	
 }
 
-// If being called via ajax, autorun the function
-//if($_POST['ajax']){ echo storeAddress(); }
-echo storeAddress();
+
+//print_r($_POST);
+$mc = new postToMailChimp();
+$api_key = get_option('nm_mc_api_key');
+$thanksMessage = get_option('nm_mc_thanks_message');
+
+if($mc -> setPostedData($_POST['email'], $_POST['list_id'], $_POST['fname'], $api_key, $thanksMessage))
+	echo $mc -> saveToList();
+else
+	echo $mc -> err;
+
 ?>
