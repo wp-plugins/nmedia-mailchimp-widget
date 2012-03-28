d45aa775dd8eb882ad4b40446c1f52425366377c
nmedia <nmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-28 08:56:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/nmedia-mailchimp-widget/tags/2.6@524669 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class.mailchimp.php
+++ class.mailchimp.php
@@ -69,5 +69,100 @@ class clsMailchimp extends MCAPI
 			 return $retval;
 		 }
 	}
+	
+	
+	
+	/*
+	** Adding new Merge Var to a list
+	*/
+	function addMergeVar($list_id, $tag, $detail)
+	{
+		$retval = $this -> mc -> listMergeVarAdd($list_id, $tag, $detail);
+		
+		if ($this -> mc -> errorCode){
+		  	//echo $this -> mc -> errorMessage;
+			return false;
+		 }
+		 else
+		 {
+			 return $retval;
+		 }
+	}
+	
+	
+	/*
+	** Deleting Merge Var from a list
+	*/
+	function XMergeVar($list_id, $tag)
+	{
+		$retval = $this -> mc -> listMergeVarDel($list_id, $tag);
+		
+		if ($this -> mc -> errorCode){
+		  	//echo $this -> mc -> errorMessage;
+			return false;
+		 }
+		 else
+		 {
+			 return $retval;
+		 }
+	}
+	
+	
+	/*
+	** Adding new Group to a list
+	*/
+	function addInterestGroup($list_id, $name)
+	{
+		$retval = $this -> mc -> listInterestGroupAdd($list_id, $name);
+		
+		if ($this -> mc -> errorCode){
+		  	//echo $this -> mc -> errorMessage;
+			return false;
+		 }
+		 else
+		 {
+			 return $retval;
+		 }
+	}
+	
+	
+	/*
+	** Adding new Group to a Interest
+	*/
+	function addGroup($list_id, $name, $groups)
+	{
+		$groups = explode(",", $groups);
+		
+		$retval = $this -> mc -> listInterestGroupingAdd($list_id, $name, "hidden", $groups);
+		
+		if ($this -> mc -> errorCode){
+		  	//echo $this -> mc -> errorMessage;
+			return false;
+		 }
+		 else
+		 {
+			 return $retval;
+		 }
+	}
+	
+	
+	
+	
+	/*
+	** Deleting Group from a list
+	*/
+	function XGroup($list_id, $name)
+	{
+		$retval = $this -> mc -> listInterestGroupDel($list_id, $name);
+		
+		if ($this -> mc -> errorCode){
+		  	//echo $this -> mc -> errorMessage;
+			return false;
+		 }
+		 else
+		 {
+			 return $retval;
+		 }
+	}
 }
 ?>
--- list-manager.php
+++ list-manager.php
@@ -8,7 +8,67 @@ include ( $mailchimp );
 
 $mc = new clsMailchimp();
 
-$arrList = $mc -> getAccountLists();
+/* ============== processing block ========================== */
+
+  					/* ====> Adding new Merge Variable <====== */
+if(isset($_POST['new-var']))
+{
+	if($mc -> addMergeVar($_GET['lid'], $_POST['new-var'], $_POST['var-detail']))
+	{
+		echo '<div class="updated"><p>Variable added into list</p></div>';
+	}
+	else
+	{
+		echo '<div class="error"><p>'.$mc -> mc -> errorMessage.'</p></div>';
+	}
+}
+
+/* ====> Deleting new Merge Variable <====== */
+if($_GET['act'] == 'del-var')
+{
+	if($mc -> XMergeVar($_GET['lid'], $_GET['tag']))
+	{
+		echo '<div class="updated"><p>Variable deleted from list</p></div>';
+	}
+	else
+	{
+		echo '<div class="error"><p>'.$mc -> mc -> errorMessage.'</p></div>';
+	}
+}					
+
+
+
+/* ====> Adding new Group <====== */
+if(isset($_POST['new-group']))
+{
+	if($mc -> addGroup($_GET['lid'], $_POST['new-group'], $_POST['new-interest']))
+	{
+		echo '<div class="updated"><p>Group added into list</p></div>';
+	}
+	else
+	{
+		echo '<div class="error"><p>'.$mc -> mc -> errorMessage.'</p></div>';
+	}
+}
+
+
+/* ====> Deleting new Merge Variable <====== */
+if($_GET['act'] == 'del-group')
+{
+	if($mc -> XGroup($_GET['lid'], $_GET['group']))
+	{
+		echo '<div class="updated"><p>Group deleted from list</p></div>';
+	}
+	else
+	{
+		echo '<div class="error"><p>'.$mc -> mc -> errorMessage.'</p></div>';
+	}
+}					
+
+/* ============== processing block ========================== */
+
+
+$arrList 	= $mc -> getAccountLists();
 //print_r($arrList);
 ?>
 
@@ -20,10 +80,10 @@ $arrList = $mc -> getAccountLists();
 <ul>
 <?php
 foreach($arrList as $list):
-$urlLoadDetail = get_admin_url('', 'admin.php?page=lists-manager').'&lid='.$list['id'];	
+$urlLoadDetail = get_admin_url('', 'admin.php?page=lists-manager').'&lid='.$list['id'].'&lname='.$list['name'];	
 
 ?>
-	<li><a href="<?php echo $urlLoadDetail?>"><?php echo $list['name']?></a></li>
+	<li class="good-links"><a href="<?php echo $urlLoadDetail?>"><?php echo $list['name']?></a></li>
 <?php
 endforeach;
 ?>
@@ -38,40 +98,190 @@ $arrVars	= $mc -> getMergeVars($_GET['lid']);
 $arrGroups	= $mc -> getListGroups($_GET['lid']);
 ?>
 
-<div class="nm_mc_div lists_data">
-<h2>List variables</h2>
+<h2>Selected List: <?php echo $_GET['lname']?></h2>
 
+<div class="nm_mc_div lists_data">
+<h3>List variables</h3>
 <ul>
+<li class="good-links">+ <a href="javascript:toggleArea('c-new-var')">Add new merge variable</a></li>
+<li id="c-new-var" style="display:none">
+
+<form action="" method="post" onsubmit="return validateMe('new-var')">
+<input placeholder="Tag name e.g: PHONE" class="nm_mc_text" type="text" name="new-var" id="new-var" /><em>Must be ALL CAPS</em><br />
+<input placeholder="Detail e.g: Phone Number" class="nm_mc_text" type="text" name="var-detail" /><br />
+<input type="submit" value="+Add" class="button" />
+</form>
+</li>
+</ul>
+
+<table width="100%" class="wp-list-table widefat fixed pages">
+<thead>
+  <tr>
+    <th width="13%">Sr No#</th>
+    <th width="26%">Tag</th>
+    <th width="39%">Detail</th>
+    <th width="22%">Delete</th>
+  </tr>
+</thead>
+
+<tbody>
 <?php
+$c=0;
 foreach($arrVars as $var):
+$urlDel = get_admin_url('', 'admin.php?page=lists-manager').'&lid='.$_GET['lid'].'&tag='.$var['tag'].'&act=del-var';	
 ?>
-	<li><a href=""><?php echo $var['name']?></a></li>
+
+  <tr>
+    <td><?php echo ++$c?></td>
+    <td><?php echo $var['tag']?></td>
+    <td><a href=""><?php echo $var['name']?></a></li></td>
+    <td><a href="javascript:confirmDel('<?php echo $urlDel?>')">Delete</a></td>
+  </tr>
+   
 <?php
 endforeach;
 ?>
-</ul>
+
+</tbody>
+</table>
+
 </div>
 
 
 <div class="nm_mc_div lists_data">
-<h2>List Groups</h2>
+<h3>List Groups</h3>
 <ul>
+<li class="good-links">+ <a href="javascript:toggleArea('c-new-group')">Add new group</a></li>
+
+<li id="c-new-group" style="display:none">
+
+<form action="" method="post" onsubmit="return validateMe('new-group')">
+<input placeholder="Group title e.g: Students" class="nm_mc_text" type="text" name="new-group" id="new-group" /><br />
+<input placeholder="Interest in (sub group)e.g: Seniors" class="nm_mc_text" type="text" name="new-interest" id="new-interest" /><br />
+<input type="submit" value="+Add" class="button" />
+</form>
+</li>
+</ul>
+
+<table width="100%" class="wp-list-table widefat fixed pages">
+<thead>
+  <tr>
+    <th width="13%">Sr No#</th>
+    <th width="39%">Name</th>
+    <th width="22%">Action</th>
+  </tr>
+</thead>
+
+<tbody>
 <?php
-if(!is_array($arrGroups))
-{
-	echo $arrGroups;
-}else
-{
+$c=0;
 foreach($arrGroups as $group):
+$urlDel = get_admin_url('', 'admin.php?page=lists-manager').'&lid='.$_GET['lid'].'&group='.$group['name'].'&act=del-group';
+
+$urlGroups = get_admin_url('', 'admin.php?page=lists-manager').'&lid='.$_GET['lid'].'&group='.$group['name'].'&gid='.$group['id'];
 ?>
-	<li><a href=""><?php echo $group['name']?></a></li>
+
+  <tr>
+    <td><?php echo ++$c?></td>
+    <td><a href=""><?php echo $group['name']?></a></li></td>
+    <td><a href="javascript:confirmDel('<?php echo $urlDel?>')">Delete</a>
+    | <a href="<?php echo $urlGroups?>">View groups</a></td>
+  </tr>
+   
 <?php
 endforeach;
-}
 ?>
+
+</tbody>
+</table>
+
+
+<!-- groups interest (sub groups) -->
+<?php
+if($_GET['gid'])
+{
+	echo '<pre>';
+	print_r($arrGroups);
+	echo '</pre>';
+	
+?>
+<h3>Groups interest (sub group)</h3>
+<ul>
+<li class="good-links">+ <a href="javascript:toggleArea('c-new-group')">Add new group</a></li>
+
+<li id="c-new-group" style="display:none">
+
+<form action="" method="post" onsubmit="return validateMe('new-group')">
+<input placeholder="Group title e.g: Students" class="nm_mc_text" type="text" name="new-group" id="new-group" /><br />
+<input placeholder="Interest in (sub group)e.g: Seniors" class="nm_mc_text" type="text" name="new-interest" id="new-interest" /><br />
+<input type="submit" value="+Add" class="button" />
+</form>
+</li>
 </ul>
+
+<table width="100%" class="wp-list-table widefat fixed pages">
+<thead>
+  <tr>
+    <th width="13%">Sr No#</th>
+    <th width="39%">Name</th>
+    <th width="22%">Action</th>
+  </tr>
+</thead>
+
+<tbody>
+<?php
+$c=0;
+foreach($arrGroups as $group):
+$urlDel = get_admin_url('', 'admin.php?page=lists-manager').'&lid='.$_GET['lid'].'&group='.$group['name'].'&act=del-group';
+
+$urlGroups = get_admin_url('', 'admin.php?page=lists-manager').'&lid='.$_GET['lid'].'&group='.$group['name'].'&gid='.$group['id'];
+?>
+
+  <tr>
+    <td><?php echo ++$c?></td>
+    <td><a href=""><?php echo $group['name']?></a></li></td>
+    <td><a href="javascript:confirmDel('<?php echo $urlDel?>')">Delete</a>
+    | <a href="<?php echo $urlGroups?>">View groups</a></td>
+  </tr>
+   
+<?php
+endforeach;
+?>
+
+</tbody>
+</table>
+<?php }?>
 </div>
 
 <?php
 endif;
 ?>
+
+<script type="text/javascript">
+function toggleArea(a)
+{
+	jQuery('#'+a).fadeToggle();
+}
+
+function validateMe(elementID)
+{
+	if(jQuery("#"+elementID).val() == ''){
+		jQuery("#"+elementID).css({'border':'#f00 1px solid'});
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+function confirmDel(url)
+{
+	var a = confirm("Do you want to remove this variable?");
+	
+	if(a)
+	{
+		window.location = url;
+	}
+}
+</script>
